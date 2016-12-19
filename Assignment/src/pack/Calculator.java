ba19d307554582e55438b12916add785fbbecb0f
developer <developer@192.168.10.4>
2016-12-19 20:06:21 +0500

    Demo also implemented Hubaib Elahi

--- Assignment/src/pack/Calculator.java
+++ Assignment/src/pack/Calculator.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Calculator
 {
-    public static void main(String args[])
+    public static void cal()
     {
         float a, b, res;
         char choice, ch;
---
+++ Assignment/src/pack/Demo.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Hubaib accounts = new Hubaib();
+			accounts.register();
+			accounts.login("test", "testt");
+	}
+
+}
